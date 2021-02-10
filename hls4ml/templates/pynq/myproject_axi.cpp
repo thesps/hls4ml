c6d64999224d41885cbc619bce477bef42f6eecb
Javier Duarte <jduarte@ucsd.edu>
2021-02-09 22:45:04 -0600

    first attempt at io_stream

--- hls4ml/templates/pynq/myproject_axi.cpp
+++ hls4ml/templates/pynq/myproject_axi.cpp
@@ -12,14 +12,9 @@ void myproject(
 
     //hls-fpga-machine-learning insert local vars
 
-    for(unsigned i = 0; i < N_IN; i++){
-        #pragma HLS unroll
-        in_local[i] = in[i]; // Read input with cast
-    }
+    //hls-fpga-machine-learning insert enqueue
 
     //hls-fpga-machine-learning insert call
-    for(unsigned i = 0; i < N_OUT; i++){
-        #pragma HLS unroll
-        out[i] = out_local[i]; // Write output with cast
-    }
+
+    //hls-fpga-machine-learning insert dequeue
 }
